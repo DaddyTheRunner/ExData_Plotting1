8a945a42c71d6b533a69d48e81c26e905a07bfbd
Daddy The Runner <DaddyTheRunner@users.noreply.github.com>
2015-01-06 12:06:59 -0500

    Initial commit of Class_Proj_1.R
    - Script reads in the whole data file
      -- Converts the first column to dates
      -- Subsets the data we need
      -- Converts the other columns to time and numerics
      -- Saves the subsetted data for later use
    
    - Beginning of first plot
      -- This code will eventually move to plot1.R

---
+++ Class_Proj_1.R
@@ -0,0 +1,45 @@
+##
+## Class_Proj_1.R
+##
+## This R script contains the code required to complete the first
+## class project in the Exploratory Data Analysis class
+##
+library(dplyr)
+library(tidyr)
+
+## Read all of the data and then subset it
+all.data <- read.table("./Data/household_power_consumption.txt",
+                       sep=";", stringsAsFactors = FALSE,
+                       header = TRUE)
+
+all.data <- tbl_df(all.data)
+all.data <- mutate(all.data, Date = as.Date(Date, format="%d/%m/%Y"))
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+subset.data <- filter(all.data, Date >= as.Date("2007-02-01"),
+                      Date <= as.Date("2007-02-02"))
+rm(all.data)
+gc()
+
+## Clean up the data
+subset.data <-
+  mutate(subset.data,
+         Time = as.POSIXct(strptime(paste(Date, Time),
+                                    format="%Y-%m-%d %H:%M:%S")))
+subset.data <- subset.data %>%
+  mutate(Global_active_power = extract_numeric(Global_active_power),
+         Global_reactive_power = extract_numeric(Global_reactive_power),
+         Voltage = extract_numeric(Voltage),
+         Global_intensity = extract_numeric(Global_intensity),
+         Sub_metering_1 = extract_numeric(Sub_metering_1),
+         Sub_metering_2 = extract_numeric(Sub_metering_2))
+
+## Save the subsetted data for reuse
+write.table(subset.data, "./Data/subset-data.csv", row.names=FALSE, col.names=TRUE,
+            sep=",")
+
+## Generate the plots
+hist(subset.data$Global_active_power, col="red",
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
